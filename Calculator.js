f7b2d30e1c653b8c25ad1a65c793288fcb4af164
Sania-Akhtar-SA <saniaakhtar1712@gmail.com>
2023-11-22 23:58:51 +0530

    all functionality done

--- Calculator.js
+++ Calculator.js
@@ -1,72 +1,31 @@
-let total=0;
-let display=0;
-let operator;
-let input = document.getElementById('inputbox')
-let buttons = document.querySelectorAll('.button');
-// let string="";
- let arr = Array.from(buttons);
- function buttonClick (value){
-    if (isNaN(value)){
-        forSymbol(value);
-    }
-    else {
-        forNumber(value);
-    }
- }
-function forSymbol(symbol){
-    switch (symbol){
-            case "AC":
-                display=0;
-                total=0;
-                break;
-            case "=":
-                if (operator === null){
-                    return;
-                }
-            case "+":
-            case "-":
-            case "*":   
-            case "/":
-                forMath(symbol);
-               break;
-}
-}
-function forMath(symbol){
-    if (display === '0'){
-        return;
-    }
-    const intDisplay = parseInt(display);
 
-    if (total === 0){
-        total = intDisplay;
-    }
-    else{
-        Operation(intDisplay);
-    }
-    operator = symbol;
-    display=0;
-}
-
- function Operation (intDisplay){
-     if (operator === '+')
-         total += intDisplay;
-     else if (operator === '-')
-         total -= intDisplay;
-     else if (operator === 'x')
-         total *= intDisplay;
-     else if (operator === '÷')
-         total /= intDisplay;
-
- }
- function forNumber(numberString){
-    if(display === '0')
-        display = numberString;
-    else
-        display += numberString;
- }
-function init(){
-    document.querySelector('.button').addEventListener('click',function (event){
-        buttonClick(event.target.innerText);
-    } )
-}
-init();
+let input = document.getElementById('inputBox');
+let buttons =document.querySelectorAll('button');
+let string="";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string= eval(string);
+            input.value= string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string= "";
+            input.value= string;
+        }
+        else {
+            if(e.target.innerHTML == 'x'){
+                e.target.innerHTML = '*'
+                string= eval(string);
+                input.value= string;
+            }
+            else if(e.target.innerHTML == '÷'){
+                e.target.innerHTML = '/'
+                string= eval(string);
+                input.value= string;
+            }
+            string += e.target.innerText;
+            input.value= string;
+        }
+    })
+ })
