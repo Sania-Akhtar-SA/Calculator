2563a461092566bd4aa4db2ad37290be4568aa92
Sania-Akhtar-SA <saniaakhtar1712@gmail.com>
2023-11-28 17:15:08 +0530

    Updated

--- Calculator.js
+++ Calculator.js
@@ -1,25 +1,25 @@
 
 let input = document.getElementById('inputBox');
-let buttons =document.querySelectorAll('button');
+let buttons =document.querySelectorAll('button'); 
 let string="";
 let arr = Array.from(buttons);
 arr.forEach(button => {
-    button.addEventListener('click', (e) =>{
+    button.addEventListener('click', (e) =>{ // Evaluate
         if(e.target.innerHTML == '='){
             string= eval(string);
             input.value= string;
         }
-        else if(e.target.innerHTML == 'AC'){
+        else if(e.target.innerHTML == 'AC'){ // Cancel 
             string= "";
             input.value= string;
         }
         else {
-            if(e.target.innerHTML == 'x'){
+            if(e.target.innerHTML == 'x'){ // As x is not considered as multiplication command in programming.
                 e.target.innerHTML = '*'
                 string= eval(string);
                 input.value= string;
             }
-            else if(e.target.innerHTML == '÷'){
+            else if(e.target.innerHTML == '÷'){ // As ÷ is not considered as division command in programming.
                 e.target.innerHTML = '/'
                 string= eval(string);
                 input.value= string;
@@ -28,4 +28,4 @@ arr.forEach(button => {
             input.value= string;
         }
     })
- })
+})
